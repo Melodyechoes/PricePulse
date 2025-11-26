d00520d25c2a782ed4a17127cab738fbfaf46fa1
Melodyechoes <1906913664@qq.com>
2025-11-26 11:38:10 +0800

    初始提交：项目基础框架

---
+++ src/main/java/com/zlb/pricepulse/PricePulseApplication.java
@@ -0,0 +1,14 @@
+package com.zlb.pricepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricePulseApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricePulseApplication.class, args);
+    }
+
+}
