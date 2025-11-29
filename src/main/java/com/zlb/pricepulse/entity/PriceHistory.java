ec98792807bdb0ff7104e11e3571286ec8f81034
Melodyechoes <122785392+Melodyechoes@users.noreply.github.com>
2025-11-29 14:30:33 +0800

    测试

---
+++ src/main/java/com/zlb/pricepulse/entity/PriceHistory.java
@@ -0,0 +1,14 @@
+package com.zlb.pricepulse.entity;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class PriceHistory {
+    private Long id;
+    private Long productId;
+    private BigDecimal price;
+    private LocalDateTime createdAt;
+}
